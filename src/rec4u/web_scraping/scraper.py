46fa22deaf1b2761ad55a870d2c79c8977cdd611
JessikaSmith <mariyaxod@yandex.ru>
2022-03-09 16:32:51 +0300

    Add simple workflow

--- src/rec4u/web_scraping/scraper.py
+++ src/rec4u/web_scraping/scraper.py
@@ -3,11 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from argparse import A
 from typing import List
 
 class Scraper:
     def __init__(self):
+        driver = webdriver.Chrome(executable_path=)
+
+        driver.get("http://www.google.com")
         raise NotImplementedError
+
+    def parse_config(self):
+        pass
 
     def keywords_searcher(self, keywords: List):
 
